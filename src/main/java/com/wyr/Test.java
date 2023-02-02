f299bedfafb24dc9a46ad174aa18baf24f029e57
wyr <2840524621@qq.com>
2023-02-02 16:03:58 +0800

    push commit1

--- src/main/java/com/wyr/Test.java
+++ src/main/java/com/wyr/Test.java
@@ -13,5 +13,6 @@ public class Test {
         System.out.println("5");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("push test");
     }
 }
